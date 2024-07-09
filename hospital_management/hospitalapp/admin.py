f70db78c865c6ef8d9aeec3cab2e31f8ce1a4f64
Dhanaprasad0 <danaprasadrongala288@gmail.com>
2024-07-09 22:45:28 +0530

    Delete hospital_management directory

--- hospital_management/hospitalapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import CustomUser, Patient, Doctor
-
-admin.site.register(CustomUser)
-admin.site.register(Patient)
-admin.site.register(Doctor)
